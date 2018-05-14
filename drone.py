c5f4555c27005ff7e6a41fccd59ff875eff5c8fc
diegoballest <diego.alfredo96@gmail.com>
2018-05-14 13:27:27 -0500

    final working drone

--- drone.py
+++ drone.py
@@ -9,15 +9,12 @@ import zipfile
 
 from collections import defaultdict
 from io import StringIO
-#from matplotlib import pyplot as plt
 from PIL import Image
 
 import cv2
 from pyardrone.video import VideoClient
 from pyardrone import ARDrone
 import time
-
-#cap = cv2.VideoCapture(0)
 
 cap = VideoClient('192.168.1.1', 5555)
 cap.connect()
@@ -27,60 +24,30 @@ cap.video_ready.wait()
 drone = ARDrone()
 drone.navdata_ready.wait()
 
-# This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
-
-
-# ## Object detection imports
-# Here are the imports from the object detection module.
-
-# In[3]:
 
 from utils import label_map_util
 
 from utils import visualization_utils as vis_util
 
-
-# # Model preparation
-
-# ## Variables
-#
-# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.
-#
-# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
-
-# In[4]:
-
-# What model to download.
 MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
 MODEL_FILE = MODEL_NAME + '.tar.gz'
 DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
-# List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 
 NUM_CLASSES = 90
 
-
-# ## Download Model
-
-# In[5]:
-
-#opener = urllib.request.URLopener()
-#opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+#MODEL
+opener = urllib.request.URLopener()
+opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
 tar_file = tarfile.open(MODEL_FILE)
 for file in tar_file.getmembers():
     file_name = os.path.basename(file.name)
     if 'frozen_inference_graph.pb' in file_name:
         tar_file.extract(file, os.getcwd())
-
-
-# ## Load a (frozen) Tensorflow model into memory.
-
-# In[6]:
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
@@ -91,64 +58,25 @@ with detection_graph.as_default():
         tf.import_graph_def(od_graph_def, name='')
 
 
-# ## Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
-# In[7]:
-
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
 
-# ## Helper code
-
-# In[8]:
-
-def load_image_into_numpy_array(image):
-    (im_width, im_height) = image.size
-    return np.array(image.getdata()).reshape(
-        (im_height, im_width, 3)).astype(np.uint8)
-
-
-# # Detection
-
-# In[9]:
-
-# For the sake of simplicity we will use only 2 images:
-# image1.jpg
-# image2.jpg
-# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = 'test_images'
-TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
-
-# Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
-
-
-# In[10]:
 flag = 0
 with detection_graph.as_default():
     with tf.Session(graph=detection_graph) as sess:
         while True:
-            #print("FPS: ",cap.frame)
             image_np = cap.frame
-            #ret, image_np = cap.read()
-            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
             image_np_expanded = np.expand_dims(image_np, axis=0)
             image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-            # Each box represents a part of the image where a particular object was detected.
             boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-            # Each score represent how level of confidence for each of the objects.
-            # Score is shown on the result image, together with the class label.
             scores = detection_graph.get_tensor_by_name('detection_scores:0')
             classes = detection_graph.get_tensor_by_name('detection_classes:0')
             num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-            # Actual detection.
             (boxes, scores, classes, num_detections) = sess.run(
               [boxes, scores, classes, num_detections],
               feed_dict={image_tensor: image_np_expanded})
-            # Visualization of the results of a detection.
             vis_util.visualize_boxes_and_labels_on_image_array(
                 image_np,
                 np.squeeze(boxes),
@@ -161,25 +89,20 @@ with detection_graph.as_default():
 
             print(list)
             for name in list:
-                if name['name'] == 'bottle' and flag == 0:
+                if name['name'] == 'bottle' or name['name'] == 'cup' and flag == 0:
                     flag = 1
-                    print("bottle detected1")
+                    print("starter object detected")
                     time.sleep(0.1)
                     while not drone.state.fly_mask:
                         drone.takeoff()
 
                     time.sleep(0.1)
-                    print("tv detected")
-                    #print(boxes[0])
-                    #[ymin, xmin, ymax, xmax]
-                        #drone.takeoff()
-                    #time.sleep(20)
-                elif name['name'] == 'cell phone' or name['name'] == 'remote':
+                elif name['name'] == 'cell phone' or name['name'] == 'remote' or name['name'] == 'skateboard' or name['name'] == 'surfboard':
                     while drone.state.fly_mask:
                         drone.land()
-                        print("phone detected")
+                        print("lander object detected")
                         flag = 0
-                if name['name'] == 'person':
+                if name['name'] == 'cup':
 
                     posY =( boxes[0][0][0]  +  boxes[0][0][2])/2
                     posX =( boxes[0][0][1]  +  boxes[0][0][3])/2
@@ -187,18 +110,23 @@ with detection_graph.as_default():
                     print('Position x' , posX)
 
                     #print(boxes[0])
-                    if posX < 0.4:
-                        print("pos 0")
+
+                    if posX < 0.2:
+                        print("very left")
+                        drone.move(ccw = 1.0)
+                    elif posX < 0.4:
+                        print("left")
                         drone.move(ccw = 0.5)
                     elif posX <= 0.60 and posX >= 0.4:
-                        print("pos 1")
-                        drone.move(forward = 0.3)
-                        drone.move(forward = 0.3)
-                        drone.move(forward = 0.3)
-                        drone.move(forward = 0.3)
+                        print("middle")
+                        drone.move(forward = 1)
                     elif posX > 0.60:
-                        print("pos 2")
+                        print("right")
                         drone.move(cw = 0.5)
+                    elif posX > 0.80:
+                        print("very right")
+                        drone.move(cw = 1.0)
+
 
 
             cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
